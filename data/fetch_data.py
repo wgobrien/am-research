cbebe835556e07a0d050d9abe2d4b4df964b0e43
Will Obrien <wgo2104@columbia.edu>
2021-06-24 17:08:04 -0400

    last commit for the day

--- data/fetch_data.py
+++ data/fetch_data.py
@@ -17,7 +17,8 @@ print("exporting data to csv...\n")
 
 # OPEN CSV AND ITERATE THROUGH RESULTS
 with open('am_data.csv', 'w', newline='') as f:
-    writer = csv.writer(f)    
+    writer = csv.writer(f)
+    writer.writerow(i[0] for i in cur.description)
     for row in cur.fetchall() :
         writer.writerow(row)
 
