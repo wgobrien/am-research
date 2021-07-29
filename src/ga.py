6d6bcfe933a1ca2e61b471517c6a8b4fd27c1b57
William Obrien <wgo2104@columbia.edu>
2021-07-29 09:09:26 -0400

    Dynamic shrinking to mutation probability in genetic algorithm

--- src/ga.py
+++ src/ga.py
@@ -15,7 +15,6 @@ class GeneticAlgorithm:
         y_scale - Scaler model for labels
         parameters - list of parameters to optimize
         boundaries - list of tuples with lower and upper bounds of parameters
-        mode - minimize or maximize input function (porosity=minimize, tensile_strength=maximize)
         pop_size - default=10, number of samples to keep in population at a time
         '''
 
@@ -85,6 +84,14 @@ class GeneticAlgorithm:
         return prediction
 
 
+    def sort_pop(self, population):
+        '''
+        Takes a list of dictionaries (parameter : value pairs) and returns 
+        sorted list by model_fitness, smallest to largest val
+        '''
+        return sorted(population, key=self.model_fitness)
+
+
     def roulette_select(self, sorted, summation):
         '''
         Selection technique that gives higher probability of selection based on highest fitness
@@ -104,13 +111,14 @@ class GeneticAlgorithm:
         draw = np.random.uniform(0, 1)
 
         range = 0
-        for individual in sorted:
+        for idx, individual in enumerate(sorted):
             fitness = self.model_fitness(individual) + offset
             p = fitness / summation
             range += p
 
             if draw <= range:
-                return individual
+                return individual, idx
+
 
     def rank_select(self, sorted, summation):
         '''
@@ -127,14 +135,26 @@ class GeneticAlgorithm:
             p = idx / summation
             range += p
             if draw <= range:
-                return individual
+                return individual, idx
 
-    def sort_pop(self, population):
+
+    def mutation(self, individual, mutation_prob):
         '''
-        Takes a list of dictionaries (parameter : value pairs) and returns 
-        sorted list by model_fitness, smallest to largest val
+        Generates mutation of indiviudal. Mutation can be modified to increase or decrease by a higher amount.
+        Mutation currently adjusts values by adding or decreasing by a random number between 5% of the mean of
+        the feature's boundaries.
+        --> Open to ideas on ways to dynamically shrink mutation probability, maybe based on rank?
         '''
-        return sorted(population, key=self.model_fitness)
+        x = 0
+        for k in individual.keys():
+            if self.dynamic:
+                bound = mutation_prob*np.mean(self.boundaries[x])
+            else:
+                bound = self.mutation_rate*np.mean(self.boundaries[x])
+            itr = individual[k] + np.random.uniform(-bound, bound)
+            individual[k] = min( max(itr, self.boundaries[x][0]), self.boundaries[x][1] )
+            x += 1
+        return individual
 
 
     def crossover(self, a, b):
@@ -146,23 +166,7 @@ class GeneticAlgorithm:
         cross = {}
         for (k,v), (_,v2) in zip(a.items(), b.items()):
             cross[k] = np.mean([v, v2])
-        return self.mutation(cross)
-
-
-    def mutation(self, individual):
-        '''
-        Generates mutation of indiviudal. Mutation can be modified to increase or decrease by a higher amount.
-        Mutation currently adjusts values by adding or decreasing by a random number between 5% of the mean of
-        the feature's boundaries.
-        --> Open to ideas on ways to dynamically shrink mutation probability, maybe based on rank?
-        '''
-        x = 0
-        for k in individual.keys():
-            bound = self.mutation_rate*np.mean(self.boundaries[x])
-            itr = individual[k] + np.random.uniform(-bound, bound)
-            individual[k] = min( max(itr, self.boundaries[x][0]), self.boundaries[x][1] )
-            x += 1
-        return individual
+        return cross
 
 
     def mating_pool(self):
@@ -178,27 +182,40 @@ class GeneticAlgorithm:
             summation = sum(range(1, self.pop_size+1))
 
         for _ in range(self.pop_size):
-            x1 = self.select(sorted, summation)
-            x2 = self.select(sorted, summation)
-            mpool.append(self.crossover(x1, x2))
+            x1, r1 = self.select(sorted, summation)
+            x2, r2 = self.select(sorted, summation)
+            
+            # Used for dynamic shrinking of mutation rate, does nothing if set
+            # to a constant value in runtime (self.dynamic will be false)
+            r1 = self.pop_size - r1 + 1
+            r2 = self.pop_size - r1 + 1
+            mutation_prob = self.mutation_rate*(np.mean([r1, r2]) / self.pop_size)
+
+            x_new = self.crossover(x1, x2)
+            mpool.append(self.mutation(x_new, mutation_prob))
 
         return mpool
 
 
-    def run(self, mode='maximize', select='rank', mutation_rate=.01, generations=500, verbose=False):
+    def run(self, mode='maximize', select='rank', mutation_rate=.01, generations=500, exploration=.1, verbose=False):
         '''
         inputs:
+            mode - minimize or maximize input function (porosity=minimize, tensile_strength=maximize)
+            select - option to choose selection technique between roulette and rank selection
             mutation_rate - have the option to set the probability at which an individual mutates
             generations - set max number of generations to run
-            select - option to choose selection technique between roulette and rank selection
+            exploration - only applicable for dynamic mutation rate, tells how much to explore vs exploit (higher will try more, might not converge as consistently)
             verbose - option to print generation #'s and populations for each generation
         output:
             Dictionary feature value set of the highest performing individual in the final population
         '''
+        self.gen = generations # save for export data
+        self.exp = exploration
+
         # set mutation probability before each run
         if mutation_rate == 'dynamic':
             self.dynamic = True
-            self.mutation_rate = .02
+            self.mutation_rate = exploration
         else:
             self.dynamic = False
             self.mutation_rate = mutation_rate
@@ -245,19 +262,41 @@ class GeneticAlgorithm:
             out = open(out_path, 'a')
         else:
             out = open(out_path, 'w')
-            out.write('=======================\nOptimal Paramter Report\n=======================\n')
-            
-        out.write(f'\n=======================================\n')
-        print(f'[{self.model}]\n---------------------------------------')
-        out.write(f'[{self.model}]\n---------------------------------------\n')
+            out.write('==============================================='
+                      '\n            Optimal Paramter Report'
+                      '\n==============================================='
+                      '\nReport with all GA runs. Shows the model, the'
+                      '\nGA run outputs, and which GA settings were used.\n')
+
+        if self.dynamic:
+            mr = f'dynamic – exploration rate: {self.exp}'
+        else:
+            mr = str(self.mutation_rate)
+
+        print('=======================================')    
+        print(f'{type(self.model).__name__} Model\n---------------------------------------')
+        out.write(
+            '\n==============================================='
+            f'\n{type(self.model).__name__} Model\n-----------------------------------------------'
+            f'\nGA Parameters\n-------------'
+            f'\nPopulation Size: {self.pop_size}'
+            f'\nGenerations: {self.gen}'
+            f'\nSelect: {self.select.__name__}'
+            f'\nMutation Rate: {mr}'
+            f'\n-----------------------------------------------\nFeatures\n--------\n'
+            )
 
         for k, v in best.items():
             print(f'{k}: {v}')
             out.write(f'{k}: {v}\n')
-        
+
+        out.write(f'-----------------------------------------------'
+            f'\nPrediction\n----------'
+            f'\nPorosity: {self.model_predict(best)}'
+            '\n===============================================\n'
+            )
         print('---------------------------------------\nPorosity:', self.model_predict(best))
-        out.write(f'---------------------------------------\nPorosity: {self.model_predict(best)}\n')
-        out.write(f'=======================================\n')
+        print('=======================================')
 
         out.close()
 
@@ -285,5 +324,9 @@ if __name__ == '__main__':
     predict = ga.model_predict({'LaserPowerHatch':300, 'LaserSpeedHatch':1200})
     
     # Run the algorithm to find optimal parameter set
-    best_performer = ga.run(generations=200, verbose=True, select='rank', mode='minimize')
+    #avg_power = 0
+    #for _ in range(10):
+    best_performer = ga.run(mode='minimize', select='rank', mutation_rate='dynamic', generations=200, exploration=.05, verbose=True)
+    #avg_power += ga.model_predict(best_performer)
     ga.export(best_performer) # best is optional, can have export run the algorithm instead
+    #print(avg_power/10)
